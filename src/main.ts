b89c7692698cd4724650ada029174db17b86c008
Murilo Machado <murilovmachado@gmail.com>
2021-03-20 04:09:40 +0000

    Fix npm run dev

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,23 @@ export class GameScene extends Phaser.Scene {
   }
  
   public update() {
-    // TODO
+    const cursorKeys = this.input.keyboard.createCursorKeys();
+
+    if (cursorKeys.up.isDown) {
+      this.square.body.setVelocityY(-500);
+    } else if (cursorKeys.down.isDown) {
+      this.square.body.setVelocityY(500);
+    } else {
+      this.square.body.setVelocityY(0);
+    }
+    
+    if (cursorKeys.right.isDown) {
+      this.square.body.setVelocityX(500);
+    } else if (cursorKeys.left.isDown) {
+      this.square.body.setVelocityX(-500);
+    } else {
+      this.square.body.setVelocityX(0);
+    }
   }
 }
 

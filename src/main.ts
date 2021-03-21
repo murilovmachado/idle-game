b2df51992d0afcb168f1c9ec10de3218060f7b16
Murilo Machado <murilovmachado@gmail.com>
2021-03-21 02:32:02 +0000

    Input working on ios

--- src/main.ts
+++ src/main.ts
@@ -8,33 +8,48 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 
 export class GameScene extends Phaser.Scene {
   private square: Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
+  private targetLocation: {x: number; y: number};
+  private VELOCITY: number = 500;
  
   constructor() {
     super(sceneConfig);
+    this.targetLocation = {x: 400, y: 400};
   }
  
   public create() {
-    this.square = this.add.rectangle(400, 400, 100, 100, 0xFFFFFF) as any;
+    this.square = this.add.rectangle(400, 400, 100, 100, 0xFF0000) as any;
     this.physics.add.existing(this.square);
+  }
+
+  private isLessThan(a, b) {
+    return b - a >= 10;
+  }
+
+  private isGreaterThan(a, b) {
+    return a - b >= 10;
   }
  
   public update() {
-    const cursorKeys = this.input.keyboard.createCursorKeys();
-
-    if (cursorKeys.up.isDown) {
-      this.square.body.setVelocityY(-500);
-    } else if (cursorKeys.down.isDown) {
-      this.square.body.setVelocityY(500);
-    } else {
-      this.square.body.setVelocityY(0);
+    const pointer = this.input.activePointer;
+    if (pointer.isDown) {
+      this.targetLocation.x = pointer.x;
+      this.targetLocation.y = pointer.y
     }
-    
-    if (cursorKeys.right.isDown) {
-      this.square.body.setVelocityX(500);
-    } else if (cursorKeys.left.isDown) {
-      this.square.body.setVelocityX(-500);
+
+    if (this.isLessThan(this.square.x,this.targetLocation.x)) {
+      this.square.body.setVelocityX(this.VELOCITY);
+    } else if (this.isGreaterThan(this.square.x, this.targetLocation.x)) {
+      this.square.body.setVelocityX(-this.VELOCITY);
     } else {
       this.square.body.setVelocityX(0);
+    }
+
+    if (this.isLessThan(this.square.y, this.targetLocation.y)) {
+      this.square.body.setVelocityY(this.VELOCITY);
+    } else if (this.isGreaterThan(this.square.y, this.targetLocation.y)) {
+      this.square.body.setVelocityY(-this.VELOCITY);
+    } else {
+      this.square.body.setVelocityY(0);
     }
   }
 }
